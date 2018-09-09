462224e6e5d7830545c9c2f730f2d9beb3c480eb
Flawden <topchiidv1998@gmail.com>
2018-09-09 14:12:53 +0300

    1)Была создана змейка, не способная двигаться.

--- GitProject/GitProject/Program.cs
+++ GitProject/GitProject/Program.cs
@@ -10,21 +10,21 @@ namespace GitProject
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            Point p2 = new Point(4, 5, '#');
 
-            p1.Draw();
-            p2.Draw();
-
-            HorizontalLine xlineUp = new HorizontalLine(1, 100, 0, '*');
-            HorizontalLine xlineDown = new HorizontalLine(1, 100, 35, '*');
-            VerticalLine ylineLeft = new VerticalLine(0, 35, 0, '*');
-            VerticalLine ylineRight = new VerticalLine(0, 35, 100, '*');
+            //Отрисовка линии
+            HorizontalLine xlineUp = new HorizontalLine(1, 118, 0, '*');
+            HorizontalLine xlineDown = new HorizontalLine(1, 118, 29, '*');
+            VerticalLine ylineLeft = new VerticalLine(0, 29, 0, '*');
+            VerticalLine ylineRight = new VerticalLine(0, 29, 118, '*');
             xlineUp.Drow();
             xlineDown.Drow();
             ylineLeft.Drow();
             ylineRight.Drow();
-            
+
+            //Отрисовка точек
+            Point p = new Point(8, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
 
             Console.ReadKey();
         }
